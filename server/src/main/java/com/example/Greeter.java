88ce541a9ddaaed7f50f7bd129db6be56afe7520
tszulist <tomasz.szulist@gmail.com>
2016-12-22 08:49:56 +0100

    JavaDoc comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,9 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  /**
+   * This is a greet message implementation.
+   */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
